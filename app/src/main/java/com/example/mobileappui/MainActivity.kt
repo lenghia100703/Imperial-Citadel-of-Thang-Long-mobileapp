459ddca0915bd12732e23e9fd40dcafba76b145a
Daohuy2409 <100114549+Daohuy2409@users.noreply.github.com>
2024-04-26 22:00:58 +0700

    create LoginActivity

--- app/src/main/java/com/example/mobileappui/MainActivity.kt
+++ app/src/main/java/com/example/mobileappui/MainActivity.kt
@@ -1,28 +1,14 @@
 package com.example.mobileappui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
-import com.example.mobileappui.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mobileappui.presentation.home.LoginActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    lateinit var username : EditText
-    lateinit var password: EditText
-    lateinit var loginButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.loginButton.setOnClickListener(View.OnClickListener {
-            if (binding.username.text.toString() == "user" && binding.password.text.toString() == "1234"){
-                Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
-            }
-        })
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
