b69e56950857a08dd3697074c3f0741b77529411
Daohuy2409 <100114549+Daohuy2409@users.noreply.github.com>
2024-05-07 00:15:50 +0700

    update menu

--- app/src/main/java/com/example/mobileappui/route/Account.kt
+++ app/src/main/java/com/example/mobileappui/route/Account.kt
@@ -1,5 +1,6 @@
 package com.example.mobileappui.route
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,6 +8,8 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 import com.example.mobileappui.R
+import com.example.mobileappui.presentation.home.LoginActivity
+
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 /**
@@ -18,7 +21,6 @@ class Account : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
