801e421b34c7f3de4df3d1450b1ea48c8042c1b2
Daohuy2409 <100114549+Daohuy2409@users.noreply.github.com>
2024-05-18 02:29:47 +0700

    update quiz

--- app/src/main/java/com/example/mobileappui/route/Search.kt
+++ app/src/main/java/com/example/mobileappui/route/Search.kt
@@ -2,6 +2,7 @@ package com.example.mobileappui.route
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.Typeface
 import android.os.Bundle
 import android.text.TextWatcher
 import android.util.Log
@@ -10,11 +11,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.webkit.WebView
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.marginRight
 import androidx.fragment.app.Fragment
 import com.example.mobileappui.R
 import com.example.mobileappui.dtos.common.PaginatedDataDto
@@ -24,26 +30,9 @@ import com.example.mobileappui.services.news.NewsService
 import retrofit2.Call
 import retrofit2.Response
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-/**
- * A simple [Fragment] subclass.
- * Use the [Search.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Search : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
-
+    private val newsService: NewsService = ApiClient.newsService
     @SuppressLint("MissingInflatedId", "UseCompatLoadingForDrawables")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -51,6 +40,7 @@ class Search : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_search, container, false)
+
         val search: EditText = view.findViewById(R.id.search)
         val btnClear: Button = view.findViewById(R.id.btnClear)
         val watcher = object: TextWatcher {
@@ -76,8 +66,7 @@ class Search : Fragment() {
         search.addTextChangedListener(watcher)
         search.setOnKeyListener(View.OnKeyListener { _, keyCode, event ->
             if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_UP) {
-                //Perform Code
-                println("Enteredddddddddddddddddddddddddddd")
+
                 // Đóng bàn phím ảo
                 val inputMethodManager = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
                 inputMethodManager?.hideSoftInputFromWindow(search.windowToken, 0)
@@ -89,11 +78,7 @@ class Search : Fragment() {
             false
         })
 
-        val hint1: TextView = view.findViewById(R.id.hint1)
-        hint1.setOnClickListener {
-            search.setText(hint1.text)
-            btnClear.visibility = View.GONE
-        }
+
 
         //search navigation
         val postBtn: TextView = view.findViewById(R.id.leftBtn)
@@ -110,17 +95,23 @@ class Search : Fragment() {
             postBtn.background = resources.getDrawable(R.drawable.left_search)
             postBtn.setTextColor(Color.Black.toArgb())
         }
-        getNews()
-        return view
-    }
-    private val newsService: NewsService = ApiClient.newsService
-    fun getNews() {
-            newsService.getAllNews(1).enqueue(object : retrofit2.Callback<PaginatedDataDto<NewsDto>> {
+        val linearLayout = view.findViewById<LinearLayout>(R.id.articleList)
+        newsService.getAllNews(1).enqueue(object : retrofit2.Callback<PaginatedDataDto<NewsDto>> {
             override fun onResponse(call: Call<PaginatedDataDto<NewsDto>>, response: Response<PaginatedDataDto<NewsDto>>) {
                 if (response.isSuccessful) {
                     val news = response.body()?.data
-                    println("These are the news")
-                    Log.d("News", news.toString())
+                    Log.d("News", "These are the news")
+                    val hint1: TextView = view.findViewById(R.id.hint1)
+                    hint1.setOnClickListener {
+                        search.setText(hint1.text)
+                        btnClear.visibility = View.GONE
+                    }
+                    news?.forEach {
+                        Log.d("News", it.title)
+
+                        createNews(it, linearLayout)
+                    }
+
                 } else {
                     Log.d("News", "Failed to get news")
                 }
@@ -130,25 +121,75 @@ class Search : Fragment() {
                 Log.d("News", t.message.toString())
             }
         })
+
+        /*val viewArticle = view.findViewById<LinearLayout>(R.id.article1)
+        viewArticle.setOnClickListener {
+            view.findViewById<LinearLayout>(R.id.searchLayout).visibility = View.GONE
+            view.findViewById<ConstraintLayout>(R.id.articleLayout).visibility = View.VISIBLE
+        }
+        view.findViewById<Button>(R.id.articleExit).setOnClickListener {
+            view.findViewById<LinearLayout>(R.id.searchLayout).visibility = View.VISIBLE
+            view.findViewById<ConstraintLayout>(R.id.articleLayout).visibility = View.GONE
+        }*/
+        return view
+    }
+    //create item for each news
+    private fun createNews(newsItem: NewsDto, layout: LinearLayout) {
+        val item = LinearLayout(context)
+        val layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        layoutParams.setMargins(0, 0, 0, 20)
+        item.layoutParams = layoutParams
+
+        //create to view image and text
+        val webView = WebView(requireContext())
+        val webViewParams = LinearLayout.LayoutParams(
+            200,200
+        )
+        webViewParams.setMargins(0, 0, 30, 0)
+        webView.layoutParams = webViewParams
+
+        webView.settings.setJavaScriptEnabled(true)
+        val htmlContent =   """
+                                            <html>
+                                                <body style="margin:0;padding:0;">
+                                            <img src="${newsItem.image}" alt="Image" style="width:100%;height:100%;"/>
+                                                </body>
+                                            </html>
+                                            """
+        webView.loadData(htmlContent, "text/html", "UTF-8")
+
+        item.addView(webView)
+
+        val linearInfo = LinearLayout(context)
+        linearInfo.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        linearInfo.orientation = LinearLayout.VERTICAL
+
+        val title = TextView(context)
+        title.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        title.text = newsItem.title
+        title.textSize = 20f
+        title.setTypeface(null, Typeface.BOLD)
+        linearInfo.addView(title)
+
+        val description = TextView(context)
+        description.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        description.text = newsItem.body
+        linearInfo.addView(description)
+
+        item.addView(linearInfo)
+        layout.addView(item)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Search.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Search().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
