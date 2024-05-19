62917232324f8d49a45ecbcbbd870ab95e9f79b1
SeaDragon1312 <hhlong1312@gmail.com>
2024-05-19 18:51:27 +0700

    new update

--- app/src/main/java/com/example/mobileappui/route/BookTicket.kt
+++ app/src/main/java/com/example/mobileappui/route/BookTicket.kt
@@ -5,18 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.mobileappui.R
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-/**
- * A simple [Fragment] subclass.
- * Use the [BookTicket.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class BookTicket : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -26,12 +22,6 @@ class BookTicket : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
-//        val buttonChangeFragment : Button = view.findViewById<Button>(R.id.paymentButton)
-//        buttonChangeFragment.setOnClickListener{
-//            replaceFragment(bookedTicket())
-//        }
-        // van loi, anh em cuu :((
     }
 
     override fun onCreateView(
@@ -42,16 +32,23 @@ class BookTicket : Fragment() {
         return inflater.inflate(R.layout.fragment_book_ticket, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val nameEditText: EditText = view.findViewById(R.id.nameEditText)
+        val phoneEditText: EditText = view.findViewById(R.id.phoneEditText)
+        val ticketQuantityEditText: EditText = view.findViewById(R.id.ticketQuantityEditText)
+
+        val buttonChangeFragment: Button = view.findViewById(R.id.paymentButton)
+        buttonChangeFragment.setOnClickListener {
+            val name = nameEditText.text.toString()
+            val phone = phoneEditText.text.toString()
+            val ticketQuantity = ticketQuantityEditText.text.toString().toInt()
+            replaceFragment(BookedTicket.newInstance(name, phone, ticketQuantity))
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BookTicket.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             BookTicket().apply {
@@ -62,7 +59,7 @@ class BookTicket : Fragment() {
             }
     }
 
-    private fun replaceFragment(fragment: Fragment){
+    private fun replaceFragment(fragment: Fragment) {
         val fragmentManager = requireActivity().supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout, fragment)
--- app/src/main/java/com/example/mobileappui/route/bookedTicket.kt
+++ app/src/main/java/com/example/mobileappui/route/bookedTicket.kt
@@ -1,58 +1,60 @@
 package com.example.mobileappui.route
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.mobileappui.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_NAME = "name"
+private const val ARG_PHONE = "phone"
+private const val ARG_TICKET_QUANTITY = "ticket_quantity"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [bookedTicket.newInstance] factory method to
- * create an instance of this fragment.
- */
-class bookedTicket : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class BookedTicket : Fragment() {
+    private var name: String? = null
+    private var phone: String? = null
+    private var ticketQuantity: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            name = it.getString(ARG_NAME)
+            phone = it.getString(ARG_PHONE)
+            ticketQuantity = it.getInt(ARG_TICKET_QUANTITY)
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_booked_ticket, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val nameTextView: TextView = view.findViewById(R.id.nameTextView)
+        val phoneTextView: TextView = view.findViewById(R.id.phoneTextView)
+        val ticketQuantityTextView: TextView = view.findViewById(R.id.ticketQuantityTextView)
+
+        nameTextView.text = name
+        phoneTextView.text = phone
+        ticketQuantityTextView.text = ticketQuantity.toString()
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment bookedTicket.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                bookedTicket().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+        fun newInstance(name: String, phone: String, ticketQuantity: Int) =
+            BookedTicket().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_NAME, name)
+                    putString(ARG_PHONE, phone)
+                    putInt(ARG_TICKET_QUANTITY, ticketQuantity)
                 }
+            }
     }
 }
